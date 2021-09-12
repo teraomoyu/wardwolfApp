3f364f4b4d7952ae70b51dc9b23ed19c5135e5d8
teraomoyu <dmntc.5299@gmail.com>
2021-09-12 23:20:10 +0900

    first commit

---
+++ wordwolf/wordwolf/MemberSetViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MemberSetViewController.swift
+//  wordwolf
+//
+//  Created by Moyu Terao on 2021/09/12.
+//
+
+import UIKit
+
+class MemberSetViewController: UIViewController {
+
+    var tappedBtnTag: Int?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("### tappedBtnTag:", tappedBtnTag as Any)
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ wordwolf/wordwolf/SellectViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SellectViewController.swift
+//  wordwolf
+//
+//  Created by Moyu Terao on 2021/09/12.
+//
+
+import UIKit
+
+class SellectViewController: UIViewController {
+
+    
+    var tagNumber = 0
+    @IBOutlet weak var foxButton: UIButton!
+    @IBOutlet weak var wolfButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func tapWolfButton(_ sender: UIButton) {
+        tagNumber = sender.tag
+        print(tagNumber)
+        performSegue(withIdentifier: "toMemberSetting", sender: tagNumber)
+    }
+    
+    @IBAction func tapFoxButton(_ sender: UIButton) {
+        tagNumber = sender.tag
+        print(tagNumber)
+        performSegue(withIdentifier: "toMemberSetting", sender: tagNumber)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            if (segue.identifier == "toMemberSetting") {
+                if let vc2 = segue.destination as? MemberSetViewController{
+                // ViewControllerのtextVC2にメッセージを設定
+                    vc2.tappedBtnTag = sender as? Int
+                }
+            }
+        }
+
+}
