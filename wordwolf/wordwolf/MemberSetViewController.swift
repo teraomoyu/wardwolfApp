da2947984dd8cb027673d6785324ca85d15ef0d3
teraomoyu <dmntc.5299@gmail.com>
2021-09-13 00:05:17 +0900

    textfield uppdate

--- wordwolf/wordwolf/MemberSetViewController.swift
+++ wordwolf/wordwolf/MemberSetViewController.swift
@@ -11,23 +11,41 @@ class MemberSetViewController: UIViewController {
 
     var tappedBtnTag: Int?
     
+    @IBOutlet weak var testLabel: UILabel!//ラベル
+     
+    @IBAction func changeStepperValue(sender: UIStepper) {
+        testLabel.text = "\(sender.value)"
+        imageRemove()
+        setNameTextField(num: Int(sender.value))
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        testLabel.text = "\(tappedBtnTag)"
         
         print("### tappedBtnTag:", tappedBtnTag as Any)
+        setNameTextField(num: tappedBtnTag!)
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setNameTextField(num: Int){
+        // 入力された数だけループします。
+        for i in 0...num{
+            let textField = UITextField(frame: CGRect(x: 60,y: 190+i*30,width: 120,height: 30)) // textFieldを作りましょう。
+            textField.backgroundColor = UIColor.red // 白いとわかりずらいので赤くしました。
+            self.view.addSubview(textField) // scrollViewにaddしましょう。
+        }
     }
-    */
 
+    func imageRemove(){
+            // self.viewの上に乗っているオブジェクトを順番に取得する
+            for v in view.subviews {
+                // オブジェクトの型がUIImageView型で、タグ番号が1〜5番のオブジェクトを取得する
+                if let v = v as? UITextField  {
+                    // そのオブジェクトを親のviewから取り除く
+                    v.removeFromSuperview()
+                }
+            }
+        }
 }
