0a05d3c24fd94430769c244e5038f17b952d0440
teraomoyu <dmntc.5299@gmail.com>
2022-03-17 14:44:53 +0900

    fix: それぞれの役割の人数設定

---
+++ wordwolf/wordwolf/PersonalViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PersonalViewController.swift
+//  wordwolf
+//
+//  Created by Moyu Terao on 2022/03/17.
+//
+
+import UIKit
+
+class PersonalViewController: UIViewController {
+    
+    var memArray = [String?]()
+    var tappedBtnTag: Int?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
