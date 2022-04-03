d5221ba3dcc00f78f219025681b80852e4f879ec
teraomoyu <dmntc.5299@gmail.com>
2022-04-03 23:58:33 +0900

    add wordLabel

--- wordwolf/wordwolf/PersonalViewController.swift
+++ wordwolf/wordwolf/PersonalViewController.swift
@@ -12,25 +12,53 @@ class PersonalViewController: UIViewController {
     
     var memArray = [String?]()
     var tappedBtnTag: Int?
+    var memInt: Int = 0
     
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var wordLabel: UILabel!
     let realm = try! Realm()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         print(memArray)
         
+//      メンバー名の表示
+        nameLabel.text = memArray[memInt]
+        print(nameLabel.text!)
         //テスト
-        let test_words = RealmWords()
-        test_words.clew = "エビ"
-        test_words.wolf = "カニ"
-        test_words.fox = "ゴキブリ"
+//        let test_words = RealmWords()
+//        test_words.clew = "エビ"
+//        test_words.wolf = "カニ"
+//        test_words.fox = "ゴキブリ"
+//
+//        
+//        try! realm.write {
+//            realm.add(test_words)
+//        }
 
+//        let results = realm.objects(RealmWords.self)
+//        print(results)
+    }
+    
+    @IBAction func tapNextButton(_ sender: Any) {
         
-        try! realm.write {
-            realm.add(test_words)
+        
+    }
+    @IBAction func tapBackButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func tapEnterButton(_ sender: Any) {
+        performSegue(withIdentifier:  "nextPerson", sender: (memArray, tappedBtnTag, memInt+1))
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "nextPerson"){
+            let data: (nextMemArray: [String?], nextBtnTag: Int, nextMemInt: Int) = (sender as? ([String], Int, Int))!
+            if let nextVC = segue.destination as? PersonalViewController{
+                nextVC.memArray = data.nextMemArray
+                nextVC.tappedBtnTag = data.nextBtnTag
+                nextVC.memInt = data.nextMemInt
+            }
         }
-
-        let results = realm.objects(RealmWords.self)
-        print(results)
     }
 }
